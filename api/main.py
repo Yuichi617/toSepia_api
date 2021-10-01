e2adb9f1a04d0cd38780e40c7d65f468c50a1890
Yuichi Sugiyama <ozwald176@gmail.com>
2021-10-01 23:44:45 +0900

    update:211001

--- api/main.py
+++ api/main.py
@@ -2,8 +2,6 @@ from flask import Flask, request, make_response, send_file, jsonify
 from flask_cors import CORS
 from PIL import Image
 from io import BytesIO
-# import numpy as np
-# import cv2
 
 app = Flask(__name__)
 CORS(app) # 全てのオリジンからのアクセスを許可
